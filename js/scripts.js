6d67caceaf46df10133d96cae54f161910a06e6e
Matt Groberg <grobergmg@gmail.com>
2019-04-12 12:44:04 -0700

    start moving function

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,6 @@
 const board= document.getElementById('grid');
+makeGrid();
+addPieces();
 
 function makeGrid(){
   for(let i=1; i<=121;i++){
@@ -8,28 +10,33 @@ function makeGrid(){
   }
 }
 
-makeGrid();
-addPieces();
-
-
 function addPieces(){
   const whiteArr=[39,49,50,51,59,60,61,62,63,71,72,73,83];
   const blackArr=[4,5,6,7,8,17,34,45,56,57,67,78,44,55,65,66,77,88,114,115,116,117,118,105];
   whiteArr.forEach(spot=>{
-    const peice=document.createElement('div');
-    peice.classList.add("circle");
-    peice.classList.add("white");
+    const piece=document.createElement('div');
+    piece.classList.add("circle");
+    piece.classList.add("white");
     const location=document.getElementById(`${spot}`);
-    location.appendChild(peice);
+    location.appendChild(piece);
     if(spot===61){
-      peice.setAttribute("id","king");
+      piece.setAttribute("id","king");
     }
   });
   blackArr.forEach(spot=>{
-    const peice=document.createElement('div');
-    peice.classList.add("circle");
-    peice.classList.add("black");
+    const piece=document.createElement('div');
+    piece.classList.add("circle");
+    piece.classList.add("black");
     const location=document.getElementById(`${spot}`);
-    location.appendChild(peice);
+    location.appendChild(piece);
+
   });
+}
+
+
+movePiece(from,to){
+  let previous=document.getElementByI(`${from}`);
+  let current=document.getElementById(`${to}`);
+  let piece=previous.children;
+
 }
